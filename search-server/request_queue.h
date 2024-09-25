92149872ca8a4550cb03d1b2f8f9a2fcebc3bd9f
Artem Vaneev <emva547@gmail.com>
2024-09-25 17:15:38 +0500

    Removed templates from the classes

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -9,11 +9,7 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
 
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        const auto result_docs = search_server_.FindTopDocuments(raw_query, document_predicate);
-        ProcessRequest(result_docs.size());
-        return result_docs;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
 
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
 
@@ -35,3 +31,10 @@ private:
     
     void ProcessRequest(int res_num);
 };
+
+template <typename DocumentPredicate>
+inline std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    const auto result_docs = search_server_.FindTopDocuments(raw_query, document_predicate);
+    ProcessRequest(result_docs.size());
+    return result_docs;
+}
